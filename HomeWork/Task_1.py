4facb84c6e01aaa236305f4eff16e78fad56157e
Alex Alhevski <37059924+AlexWhocan@users.noreply.github.com>
2021-09-28 01:34:03 +0300

    Add PythonEssentials homework (#1)

---
+++ HomeWork/Task_1.py
@@ -0,0 +1,9 @@
+# Task: Create a programm, that will calculare volume of a sphere by its radius(google the formula, use Pi=3.14). Input and output from console.
+# Input: float number.
+# Output: volume of a sphere(float number).
+#
+# Example:
+#   Enter radius: 4.5
+#   Volume of a spehere with radius 4 is 381.51
+#
+# Write your solution below.
---
+++ HomeWork/Task_3.py
@@ -0,0 +1,10 @@
+# Task: Create algorithm athat accepts a number and returns a sqaure root of the number. (Google the algorythm).
+# Do not use math functions, implement any desired matimatical algorithm. Input and output from console.
+# Input: integer number
+# Output: square root of the number
+#
+# Example:
+#   Enter number: 144
+#   Square root of 144 is 12
+#
+# Write your solution below.
