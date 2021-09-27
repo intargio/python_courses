37b341144f63fa39cddc568a2af813e45184270f
Alex Alhevski <37059924+AlexWhocan@users.noreply.github.com>
2021-09-28 01:30:46 +0300

    Create Task_2.py

---
+++ HomeWork/Task_2.py
@@ -0,0 +1,12 @@
+# Task: Create a prigram that prints a rectangle with X width and Y heinght using "X" symbol.
+# Input: integer X, integer Y.
+# Output: rectange out of X'es.
+#
+# Example:
+#   Enter height: 3
+#   Enter width: 4
+#   XXXX
+#   X  X
+#   XXXX
+#
+# Write your solution below.
