a6b8c71ea1248a2fe0933b27387a0aefcea484fa
Alex Alhevski <37059924+AlexWhocan@users.noreply.github.com>
2021-09-29 11:28:51 +0300

    Create Task1.py

---
+++ HomeWork/2_AlgorythmsAndFunctions/Task1.py
@@ -0,0 +1,8 @@
+# Create a function that accepts an array of integers 
+# and returns quantity of odd numbers in it.
+# Define the array in code, pass it to the function and
+# print the result into terminal.
+# 
+# Example:
+# [1, 2, 3, 4, 5], Number of odds: 3
+# [5, 6, 6, 7, 7], Number of odds: 3
